6a1ab93d40926620c08051305164c6d51a784b95
jarano93 <jarano93@gmail.com>
2015-11-18 18:13:12 -0800

    Unsatisfactory
    Midrow's no-prop algorithm is eh....

--- pymod/net.py
+++ pymod/net.py
@@ -34,7 +34,7 @@ def _sum_sq_err(input, target):
 class Net:
     """defines a neural network object"""
 
-    def __init__(self, input_len, output_len, layer0_len, layer1_len, layer2_len, SMD=True):
+    def __init__(self, input_len, output_len, layer0_len, layer1_len, layer2_len, CGD=True):
         """This instantiates a neural network
         
         Args:
@@ -58,28 +58,15 @@ class Net:
         self.hmap1 = np.zeros(layer1_len + 1)
         self.hmap2 = np.zeros(layer2_len + 1)
 
-        # self.weight0 = np.ones((input_len + 1, layer0_len))
-        # self.weight1 = np.ones((layer0_len + 1, layer1_len))
-        # self.weight2 = np.ones((layer1_len + 1, layer2_len))
-        # self.weight3 = np.ones((layer2_len + 1, output_len))
-
-        self.weight0 = 2 * np.random.random_sample((input_len + 1, layer0_len)) - 1 
-        self.weight1 = 2 * np.random.random_sample((layer0_len + 1, layer1_len)) - 1
-        self.weight2 = 2 * np.random.random_sample((layer1_len + 1, layer2_len)) - 1
-        self.weight3 = 2 * np.random.random_sample((layer2_len + 1, output_len)) - 1
+        self.weight0 = np.random.random_sample((input_len + 1, layer0_len))
+        self.weight1 = np.random.random_sample((layer0_len + 1, layer1_len))
+        self.weight2 = np.random.random_sample((layer1_len + 1, layer2_len))
+        self.weight3 = np.random.random_sample((layer2_len + 1, output_len))
         
-        self.SMD = SMD
+        self.CGD = CGD
+        if self.CGD:
+            print "fug"
         self.__zero()
-        if SMD:
-            self.step0 = 2 * np.random.random_sample((input_len + 1, layer0_len)) - 1
-            self.step1 = 2 * np.random.random_sample((layer0_len + 1, layer1_len)) - 1
-            self.step2 = 2 * np.random.random_sample((layer1_len + 1, layer2_len)) - 1
-            self.step3 = 2 * np.random.random_sample((layer2_len + 1, output_len)) - 1
-
-            self.aux0 = 2 * np.random.random_sample((input_len + 1, layer0_len)) - 1
-            self.aux1 = 2 * np.random.random_sample((layer0_len + 1, layer1_len)) - 1
-            self.aux2 = 2 * np.random.random_sample((layer1_len + 1, layer2_len)) - 1
-            self.aux3 = 2 * np.random.random_sample((layer2_len + 1, output_len)) - 1
 
     def feedforward(self, input): # ok
         """runs the net with given input and saves hidden activations"""
@@ -106,14 +93,9 @@ class Net:
         
         for m in range(self.output_len):
             self.output[m] = np.vdot(self.weight3[:,m], self.hmap2)
-            # if self.output[m] < 0:
-                # self.output[m] = -1
-            # else:
-                # self.output[m] = 1
         # print self.output
-        return self.output
 
-    def __backprop(self, target): #  not ok
+    def __backprop(self, target): # ok
         l_product = np.zeros(self.layer2_len)
         k_product = np.zeros(self.layer1_len)
         j_product = np.zeros(self.layer0_len)
@@ -149,18 +131,18 @@ class Net:
         # print self.g_weight0
 
     def __r_pass(self):
-        for j in range(self.layer0_len):
+        for j in range(self.layer0_len + 1):
             for i in range(self.input_len + 1):
                 self.r_activation0[j] += self.g_weight0[i,j] * self.input[i]
             self.r_hmap0[j] = _dtanh(self.activation0[j]) * self.r_activation0[j]
 
-        for k in range(self.layer1_len):
+        for k in range(self.layer1_len + 1):
             for j in range(self.layer0_len + 1):
                 self.r_activation1[k] += self.weight1[j,k] * self.r_hmap0[j]
                 self.r_activation1[k] += self.g_weight1[j,k] * self.hmap0[j]
             self.r_hmap1[k] = _dtanh(self.activation1[k]) * self.r_activation1[k]
 
-        for l in range(self.layer2_len):
+        for l in range(self.layer2_len + 1):
             for k in range(self.layer1_len + 1):
                 self.r_activation2[l] += self.weight2[k,l] * self.r_hmap1[k]
                 self.r_activation2[l] += self.g_weight2[k,l] * self.hmap1[k]
@@ -198,7 +180,7 @@ class Net:
                 self.r_delta0[j] += tail_product * self.g_weight1[j,k] 
 
         for m in range(self.output_len):
-            for l in range(self.layer2_len + 1):
+            for l in range(self.layer2_len):
                 self.r_weight3[l,m] = self.r_delta3[m] * self.hmap2[l]
                 self.r_weight3[l,m] += self.delta3[m] * self.r_hmap2[l]
 
@@ -206,26 +188,32 @@ class Net:
             lead_product = self.r_delta2[l] * _dtanh(self.activation2[l])
             lead_product += self.delta2[l] * _ddtanh(self.activation2[l]) * self.r_activation2[l]
             tail_product = self.delta2[l] * _dtanh(self.activation2[l])
-            for k in range(self.layer1_len + 1):
-                self.r_weight2[k,l] = lead_product * self.hmap1[k]
-                self.r_weight2[k,l] += tail_product * self.r_hmap1[k]
+            for k in range(self.layer1_len):
+                self.r_weight2[k,l] = lead_product * self.hmap2[k]
+                self.r_weight2[k,l] += tail_product * self.r_hmap2[k]
 
         for k in range(self.layer1_len):
             lead_product = self.r_delta1[k] * _dtanh(self.activation1[k])
-            lead_product += self.delta1[k] * _ddtanh(self.activation1[k]) * self.r_activation1[k]
+            lead_prodcut += self.delta1[k] * _ddtanh(self.activation1[k]) * self.r_activation1[k]
             tail_porduct = self.delta1[k] * _dtanh(self.activation1[k])
-            for j in range(self.layer0_len + 1):
-                self.r_weight1[j,k] = lead_product * self.hmap0[j]
-                self.r_weight1[j,k] += tail_product * self.r_hmap0[j]
+            for j in range(self.layer0_len):
+                self.r_weight1[j,k] = lead_product * self.hmap1[j]
+                self.r_weight1[j,k] += tail_product * self.r_hmap1[j]
 
         for j in range(self.layer0_len):
             lead_product = self.r_delta0[j] * _dtanh(self.activation0[j])
             lead_product += self.delta0[j] * _ddtanh(self.activation0[j]) * self.r_activation0[j]
-            for i in range(self.input_len + 1):
+            for i in range(self.input_len):
                 self.r_weight0[i,j] = lead_product * self.input[i]
 
     def __weight_GD(self,step_size):
+        # print self.weight0
+        # input("Enter...")
+        # print self.g_weight0
+        # input("Enter...")
         self.weight0 -= step_size * self.g_weight0
+        # print self.weight0
+        # input("Enter...")
 
         self.weight1 -= step_size * self.g_weight1
 
@@ -233,100 +221,34 @@ class Net:
 
         self.weight3 -= step_size * self.g_weight3
 
-    def __weight_SMD(self, learn, forget): #Stochastic Meta Descent
-        for i in xrange(self.input_len + 1):
-            for j in xrange(self.layer0_len):
-                self.step0[i,j] *= max(0.5, 1 + learn * self.aux0[i,j] * self.g_weight0[i,j])
-
-        for j in xrange(self.layer0_len + 1):
-            for k in xrange(self.layer1_len):
-                self.step1[j,k] *= max(0.5, 1 + learn * self.aux1[j,k] * self.g_weight1[j,k])
-
-        for k in xrange(self.layer1_len + 1):
-            for l in xrange(self.layer2_len):
-                self.step2[k,l] *= max(0.5, 1 + learn * self.aux2[k,l] * self.g_weight2[l,l])
-
-        for l in xrange(self.layer2_len + 1):
-            for m in xrange(self.output_len):
-                self.step3[l,m] *= max(0.5, 1 + learn * self.aux2[l,m] * self.g_weight3[l,m])
-
-        self.weight0 += self.step0 * self.g_weight0
-        self.weight1 += self.step1 * self.g_weight1
-        self.weight2 += self.step2 * self.g_weight2
-        self.weight3 += self.step3 * self.g_weight3
-
-        inner = self.g_weight0 + forget * np.dot(self.aux0, np.dot(self.g_weight0.T, self.r_weight0))
-        self.aux0 = forget * self.aux0 + (self.step0 * inner)
-
-        inner = self.g_weight1 + forget * np.dot(self.aux1, np.dot(self.g_weight1.T, self.r_weight1))
-        self.aux1 = forget * self.aux1 + (self.step1 * inner)
-
-        inner = self.g_weight2 + forget * np.dot(self.aux2, np.dot(self.g_weight2.T, self.r_weight2))
-        self.aux2 = forget * self.aux2 + (self.step2 * inner)
-
-        inner = self.g_weight3 + forget * np.dot(self.aux3, np.dot(self.g_weight3.T, self.r_weight3))
-        self.aux3 = forget * self.aux3 + (self.step3 * inner)
-
-    def __weight_CGD(self): # this is broken as hell
-        denominator = np.dot(self.r_weight0.T, self.g_weight0)
-        numerator = -np.dot(self.g_weight0.T, self.g_weight0)
-        numerator -= np.dot(self.r_weight0.T, self.weight0)
-        step = la.lstsq(denominator, numerator)[0]
-        # print step
-        # print "\n"
-        # print self.weight0
-        # print "\n"
-        change = np.dot(-self.g_weight0, step)
-        # print change
-        # print "\n"
-        self.weight0 += change
-        # print self.weight0
-        # raw_input("Enter...")
+    def __weight_CGD(self):
+        direction0 = -self.g_weight0
+        denom0 = np.Matrix(self.r_weight0).T * direction0
+        numer0 = direction0 % self.g_weight0
+        numer0 += self.r_weight0 % -self.weight0
+        step0 = numer0 / denom0
+        self.weight0 += direction0 * step0
  
-        denominator = np.dot(self.r_weight1.T, self.g_weight1)
-        numerator = np.dot(self.g_weight1.T, self.g_weight1)
-        numerator += np.dot(self.r_weight1.T, self.weight1)
-        step = la.lstsq(denominator, numerator)[0]
-        # print step
-        # print "\n"
-        # print self.weight1
-        # print "\n"
-        change = np.dot(-self.g_weight1, step)
-        # print change
-        # print "\n"
-        self.weight1 += change
-        # print self.weight1
-        # raw_input("Enter...")
+        direction1 = -self.g_weight1
+        denom1 = self.r_weight1 % direction1
+        numer1 = direction1 % self.g_weight1
+        numer1 += self.r_weight1 % -self.weight1
+        step1 = numer1 / denom1
+        self.weight1 += direction1 * step1
  
-        denominator = np.dot(self.r_weight2.T, self.g_weight2)
-        numerator = np.dot(self.g_weight2.T, self.g_weight2)
-        numerator += np.dot(self.r_weight2.T, self.weight2)
-        step = la.lstsq(denominator, numerator)[0]
-        # print step
-        # print "\n"
-        # print self.weight2
-        # print "\n"
-        change = np.dot(-self.g_weight2, step)
-        # print change
-        self.weight2 += change
-        # print self.weight2
-        # print "\n"
-        # raw_input("Enter...")
+        direction2 = -self.g_weight2
+        denom2 = self.r_weight2 % direction2
+        numer2 = direction2 % self.g_weight2
+        numer2 += self.r_weight2 % -self.weight2
+        step2 = numer2 / denom2
+        self.weight2 += direction2 * step2
  
-        denominator = np.dot(self.r_weight3.T, self.g_weight3)
-        numerator = np.dot(self.g_weight3.T, self.g_weight3)
-        numerator += np.dot(self.r_weight3.T, self.weight3)
-        step = la.lstsq(denominator, numerator)[0]
-        # print step
-        # print "\n"
-        # print self.weight3
-        # print "\n"
-        change = np.dot(-self.g_weight3, step)
-        # print  change
-        self.weight3 += change
-        # print self.weight3
-        # print "\n"
-        # raw_input("Enter...")
+        direction3 = -self.g_weight3
+        denom3 = self.r_weight3 % direction3
+        numer3 = direction3 % self.g_weight3
+        numer3 += self.r_weight3 % -self.weight3
+        step3 = numer3 / denom3
+        self.weight3 += direction3 * step3
  
     def __train_N(self, input, target, N, opt_func, *func_args):
         result = self.feedforward(input)
@@ -340,8 +262,8 @@ class Net:
             print "%iteration: %i\tSumSquareError: %f" % (i, _sum_sq_err(result, target))
     
     def train_N(self, input, target, N):
-        if self.SMD:
-            self.__train_N_SMD(input, target, N)
+        if self.CGD:
+            self.__train_N_CGD(input, target, N)
         else:
             self.__train_N_GD(input, target, N)
 
@@ -352,9 +274,9 @@ class Net:
     def train_once(self, input, target, verbose=False, **kwarg):
         result = self.feedforward(input)
         self.__backprop(target)
-        if self.SMD:
+        if self.CGD:
             self.__r_pass()
-            self.__weight_SMD(kwarg['learn'], kwarg['forget'])
+            self.__weight_CGD()
         else:
             self.__weight_GD(kwarg['step_size'])
         self.feedforward(input)
@@ -365,8 +287,8 @@ class Net:
     def __train_N_GD(self, input, target, N, step_size):
         self.__train_N(input, target, N, self.__weight_GD, step_size)
 
-    def __train_N_SMD(self, input, target, N):
-        self.__train_N(input, target, N, self.__weight_SMD)
+    def __train_N_CGD(self, input, target, N):
+        self.__train_N(input, target, N, self.__weight_CGD)
 
     def __zero(self):
         self.delta0 = np.zeros(self.layer0_len)
@@ -379,7 +301,7 @@ class Net:
         self.g_weight2 = np.zeros((self.layer1_len + 1, self.layer2_len))
         self.g_weight3 = np.zeros((self.layer2_len + 1, self.output_len))
 
-        if self.SMD:
+        if self.CGD:
             self.r_activation0 = np.zeros(self.layer0_len + 1)
             self.r_activation1 = np.zeros(self.layer1_len + 1)
             self.r_activation2 = np.zeros(self.layer2_len + 1)
@@ -395,8 +317,7 @@ class Net:
             self.r_delta2 = np.zeros(self.layer2_len)
             self.r_delta3 = np.zeros(self.output_len)
 
-            # a misnomer honestly, are Hessian * g_weight
-            self.r_weight0 = np.zeros((self.input_len + 1, self.layer0_len)) 
+            self.r_weight0 = np.zeros((self.input_len + 1, self.layer0_len))
             self.r_weight1 = np.zeros((self.layer0_len + 1, self.layer1_len))
             self.r_weight2 = np.zeros((self.layer1_len + 1, self.layer2_len))
             self.r_weight3 = np.zeros((self.layer2_len + 1, self.output_len))
--- tester.py
+++ tester.py
@@ -1,72 +1,39 @@
 import numpy as np
 import random as r
-import time
-# from multiprocessing import Pool
 from pymod.net import Net
 
 data = np.genfromtxt('krkopt.csv', delimiter=',')
-
-reps = 100
-
-performance = []
+TOL = [10, 1, 1e-1, 1e-2, 1e-3]
 train_size = 50
-test_size = 10000
+test_size = 1000
 
 total_elems = range(len(data))
 train_elems = r.sample(total_elems, train_size)
 test_elems = [x for x in total_elems if x not in train_elems]
 
-# pool = Pool(processes=4)
-
-
+test_net = Net(len(data[0,0:-2]), 1, 4e3, 1e3, 1e3, False)
 single_err = 0
-
-start = time.time()
-for rep in range(reps):
-    test_net = Net(len(data[0,0:-2]), 1, 5e2, 1e3, 20, False)
-    for n in xrange(1000):
+for t in range(len(TOL)):
+    for n in xrange(40):
         train_err = 0
-        accuracy = 0
         for i in train_elems:
             single_err = test_net.err(data[i,0:-2], data[i,-1])
-            output = test_net.feedforward(data[i,0:-2])[0]
-            if abs(output - data[i,0]) < 1:
-                accuracy += 1
-            # print "SINGLE RUN ERR: %f" % (single_err)
+            print "TOL: %.3f\tSINGLE RUN ERR: %f" % (TOL[t], single_err)
             train_err += single_err
         train_err /= len(train_elems)
-        accuracy = float(accuracy) / train_size
-        # print "MEAN RUN ERR: %f\n" % (train_err)
-        # print "MEAN RUN ACC: %f\n" % (accuracy)
-        if accuracy > 0.85 and train_err < 5e-4:
+        print "\nTOL: %.3f\tMEAN RUN ERR: %f\n" % (TOL[t], train_err)
+        if train_err < TOL[t]:
             break
-        for i in r.sample(train_elems, 10):
-            test_net.train_once(data[i,1:-1], data[i,0], False, step_size=5e-4)
-            # print "output: %s\ttarget: %s" % (str(test_net.feedforward(data[i,1:-1])[0]), str(data[i,0]))
+        for i in r.sample(train_elems, 15):
+            test_net.train_once(data[i,0:-2], data[i,-1], True, step_size=7e-4)
 
     test_err = 0
-    accuracy = 0
-    output = 0
-    for i in r.sample(test_elems, test_size):
+    for i in r.sample(test_elems, 1000):
         single_err = test_net.err(data[i,0:-2], data[i,-1])
-        # print "SINGLE TEST ERR: %f" % (single_err)
-        output = test_net.feedforward(data[i,0:-2])[0]
-        if abs(output - data[i,-1]) < 1:
-            accuracy += 1
-            # print accuracy
-        # print "output: %s\ttarget: %s" % (str(output), str(data[i,0]))
+        print "TOL: %.3f\tSINGLE RUN ERR: %f" % (TOL[t], single_err)
         test_err += single_err
     test_err /= test_size
-    accuracy = float(accuracy) / test_size
-    performance.append(accuracy)
-    print "\nN: %i" % (rep)
-    print "MEAN TEST ERR: %f" % (test_err)
-    print "PERCENT ACCURACY: %f" % (accuracy)
-    print "TRAINS: %i" % (n)
-    print "RUNNING ACCURACY: %f" % (float(sum(performance)) / len(performance))
-    print "RUNNING TIME: %f" % ((time.time() - start) / len(performance))
+    print "\nTOL: %.3f\tMEAN RUN ERR: %f\n" % (TOL[t], test_err)
     # raw_input("Press Enter to continue")
-print "TOTAL MEAN ACCURACY: %f" % (float(sum(performance)) / len(performance))
-print "AVERAGE TIME: %f" % ((time.time() - start) / len(performance))
 raw_input("Pres Enter to quit...")
 exit()
