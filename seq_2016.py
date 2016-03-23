f7cd8c26dcc840b545080d019eb64ba1f55167d8
jarano93 <jarano93@gmail.com>
2016-03-23 14:41:08 -0700

    generating new stuff

--- seq_2016.py
+++ seq_2016.py
@@ -15,21 +15,22 @@ print "%d unique characters in dataset\n\n" % uni_chars
 
 # model params
 weight_scale = 1e-1
-layers = [60, 60, 60, 60]
+layers = [80, 80, 80]
 
 
 rnn = RNN(uni_chars, layers, weight_scale, False)
 rnn.set_freq(100)
 rnn.set_sample_len(400)
-rnn.set_rollback(200)
-rnn.set_padd(20)
+rnn.set_rollback(50)
+rnn.set_padd(3)
 rnn.set_clip(10)
 rnn.set_codec(cc)
 rnn.train_N(int_dataset, 2e5)
+rnn.set_padd(20)
+rnn.set_clip(5)
+rnn.cont_N(int_dataset, 3e5)
 rnn.set_padd(50)
-rnn.train_N(int_dataset, 3e5)
-rnn.set_padd(200)
-rnn.train_N(int_dataset, 5e5)
+rnn.cont_N(int_dataset, 5e5)
 f = open('rnn2016.bin', 'wb')
 pickle.dump(rnn, f)
 f.close()
